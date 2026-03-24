5aab70429577874bcb9bbefe7b94b533dba7a5c7
Your Name <you@example.com>
2026-03-24 20:33:23 +0700

    update

--- Notes/2. XML/Notes.sql
+++ Notes/2. XML/Notes.sql
@@ -7,33 +7,47 @@
 --	+ Support querying XML data through XQuery.
 --	+ Enhancements to OPENROWSET to allow bulk loading of XML data.
 --	+ The ability to parse between relational data and XML data
---	+ XML columns can be compressed and indexed.
+--	+ XML columns can be compressed (starting with SQL Server 2022) and indexed.
 --	+ Preserve the document order and document structure, elements and attributes values, namespace prefixes, and XML declaration.
 --	+ The structure is effecient for fine-grained query like extracting some of the sections within XML document using predicate evaluation.
 --	+ Effectively modifying/inserting new sections without replacing whole document, and the oprerations are performed in a transacted way.
 --	+ Guarantee the XML data format by validate input with XML schemas.
 --	+ Indexes on XML data type increases the query processing.
 --	+ More flexible than relational structure if the structure is frequently changed.
--- 3. XML storage options:
+-- 2. XML storage options:
 --	+ Natively store xml data type, improves parsing speed significantly through PSVI
 --	+ Using an annotated schema (AXSD) to decompose XML into columns, create a copy of XML data (called XML view) is stored at the relational level.
 --	+ Data is stored as large object storage, [n]varchar(max) and varbinary(max) which create an identical copy of data. 
--- 4. Typed XML instance has a collection of XML schema associated with it, provide more benefits compared to untyped XML.
+-- 3. Typed XML instance has a collection of XML schema associated with it, provide more benefits compared to untyped XML.
 --	+ The XML schema collection can be associated with variables, parameters, or columns of the xml data type.
 --	+ The ability to define validation constraints when creating or modifying XML instance.
 --	+ Store information of data types of attributes and elements in an instance, make it more adventages to query the know type of data with more precise operational semantics.
 --	+ Allow declaring an XML instance with a specific facet are DOCUMENT and CONTENT, CONTENT is the default option.
 --	+ Document XML restricts the xml instance to have exactly one top-level element.
 --	+ Content XML can contain zero or many top-level elements or even text nodes.
--- 5. SQL Server stores and return XML data in Unicode (UTF-16) encoding
+-- 4. SQL Server stores and return XML data in Unicode (UTF-16) encoding
 --	+ If a source code page does not specify the encoding in the string, an its encoding isn't Unicode, the parsing could cause error
---	+ To explicitly specify encoding for a source code page, prepending XML declaration with encoding to the XML document
+--	+ To explicitly specify encoding for a source code page, prepending XML declaration with encoding to the XML document.
+-- 5. XML Index:
+--	+ Indexing XML data will affect all tags, values and paths over the XML instance.
+--	+ Improve query performance but trade off with index maintenance cost.
+--	+ XML indexes are defined as categories: Primary XML index and Secondary XML index
+--	+ The primary index is a shredded representation of XML instance, it create a row for each node in XML object
+--		+ Rows store all informations of a node: tag/element/attribute name, node type and its value, document order, path from node to the root
+--		+ This stored information help query engine to quickly locate the specific node through its name and path.
+--		+ Persist the representation of XML, avoid shredding XML binary large objects at run time.
+--	+ The seondary indexes store additional data of XML, they are divided into three main types:
+--		+ PATH secondary XML index: significantly speed up query that searching for paths
+--		+ VALUE secondary XML index: benefits from querying specific node value without knowing the element and attribute names of the node, and the path is not fully specified or it includes a wildcard.
+--		+ PROPERTY secondary XML index: 
+-- 6. XML compression
+--	+ 
 
 
 
 
 -- PRACTICE
-USE AdventureWorks2014
+USE AdventureWorks2025
 GO
 
 
@@ -103,7 +117,7 @@ DECLARE @x1 XML (DOCUMENT Production.ProductDescriptionSchemaCollection) ;
 DECLARE @x2 XML (CONTENT Production.ProductDescriptionSchemaCollection);
 GO
 
-CREATE TABLE T1(
+CREATE TABLE T2(
 	Col1 Int,
 	Col2 XML (Production.ProductDescriptionSchemaCollection) NOT NULL DEFAULT CAST(N'<element1></element1>' AS XML)
 )
